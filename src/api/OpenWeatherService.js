ed0682a596d3055f88227ceb33dc346800ecfc2c
manohar_nadiminti <manoharnadiminti10@gmail.com>
2025-03-10 19:13:34 +0530

    Updated files

--- src/api/OpenWeatherService.js
+++ src/api/OpenWeatherService.js
@@ -1,7 +1,7 @@
 const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
 const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
-const WEATHER_API_KEY = 'Your API KEY';
+const WEATHER_API_KEY = 'ef1555548212106b73e6570a17e1c3ec';
 
 const GEO_API_OPTIONS = {
   method: 'GET',
